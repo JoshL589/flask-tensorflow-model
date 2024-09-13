739d09c2141bd4862d8a7fc536eb12184b01d17e
Joshua Li <joshuazli2020@gmail.com>
2024-09-13 11:04:33 +1000

    fixing server

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
+CORS(app)
 
 # Load your Tensorflow model
 model = tf.keras.models.load_model("oldmodel.h5")
