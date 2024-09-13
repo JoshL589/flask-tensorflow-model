ad128ea683252f3ab4b67fc317911e2d6d19e2a3
Joshua Li <joshuazli2020@gmail.com>
2024-09-13 10:57:27 +1000

    fix

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 import tensorflow as tf
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Load your Tensorflow model
